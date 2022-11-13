750a5185cef97c1c995718a2300af2b5ff1d3115
Haider Ali Salhi <salhiali197@gmail.com>
2022-11-13 10:22:10 +0100

    fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,41 +10,15 @@
         <div class="t-product-wrap">
             <div class="row">
 
-                <div class="col-lg-3 order-1 order-lg-0">
-                    <aside class="product-sidebar">
-                        <div class="widget">
-                            <div class="single-product-widget mb-30">
-                                <h4 class="p-sidebar-title text-right">القائمة الرئيسية</h4>
-                                <div class="p-sidebar-cat">
-                                    <ul class="text-right">
-                                        <li><a class=" active_profil  " href="{{route('home')}}"
-                                                class="active"><i class="fa fa-user float-left"></i> الحساب </a></li>
-                                        <li><a class=" " href="{{route('reclamation.index')}}"> <i
-                                                    class="fa fa-list float-left"></i> الشكاوي</a></li>
-                                        <li><a class=" " href="#"><i
-                                                    class="fa fa-book-open float-left"></i> قائمة الإنتظار</a></li>
-                                        <li><a class=" " href="#"><i
-                                                    class="fa fa-key float-left"></i>كلمة السر</a></li>
-                                        <li><a onclick="return confirm(' هل انت متاكد  ?')"
-                                                href="#"><i
-                                                    class="fa fa-power-off float-left"></i>تسجيل الخروج</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </aside>
-                </div>
+            @include('includes.modals.side')
 
                 <div class="col-lg-9 order-2 order-lg-1">
                     <div class="col-md-12">
-                    </div>
-                    <div class="row card">
+                        <div class="row card">
                         <form action="{{route('user.update',['user'=>Auth::user()->id ?? ''])}}" method="POST" class=" p-5"
-                            enctype="multipart/form-data">
-                            @csrf
+                                enctype="multipart/form-data">
+                                @csrf
                             <div class="row text-right">
-
-
                                 <div class="form-group col-md-6">
                                     <label class="font-weight-bold d-block " for="input-last-name">
                                         <span> اللقب</span>
@@ -81,12 +55,13 @@
                                 </div>
 
 
-
                                 <div class="form-group d-block m-auto text-center">
                                     <button type="submit" class="btn btn-primary ">حفظ</button>
                                 </div>
 
                         </form>
+                    </div>
+
                     </div>
                 </div>
 
